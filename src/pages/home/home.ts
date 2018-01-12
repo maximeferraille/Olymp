086572029bb1bb1df882c40d69fb355a20e212c1
Unknown <maxime.ferraille@gmail.com>
2018-01-12 16:11:41 +0100

    Change root navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+import { App, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public viewCtrl: ViewController,
+    public appCtrl: App
+  ) {}
 
+  goToOtherPage() {
+    let nav = this.appCtrl.getRootNav();
+    nav.setRoot(TabsPage);
+    // this.push(MarketPage);
   }
 
 }
