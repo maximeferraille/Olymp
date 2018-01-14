8794cd5b49f816e1d688e7381a5c927589661aaa
Unknown <maxime.ferraille@gmail.com>
2018-01-14 15:48:05 +0100

    information popup [WIP]

--- src/pages/last-deal/last-deal.ts
+++ src/pages/last-deal/last-deal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the LastDealPage page.
@@ -15,11 +16,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LastDealPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+  }
+
+  informationAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Information',
+      subTitle: 'Last Deal allow you to buy the tickets sold at the last minute. When a sale is open, it is indicated by an arrow. When it is closed by a bell. Click on the bell to receive alerts on this sale',
+      buttons: ['Dismiss']
+    });
+    alert.present();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LastDealPage');
+  }
+
+  ionViewWillEnter() {
+    var informationPopup = window.localStorage.getItem('informationPopup');
+
+    if (informationPopup != "1") {
+      this.informationAlert();
+    }
+    // else {
+    //   window.localStorage.setItem('informationPopup',1);
+    // }
   }
 
 }
