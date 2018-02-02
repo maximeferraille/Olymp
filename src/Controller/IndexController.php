9f4eab3a18eba12c99f03aa3f580d4e2e376ffae
Jibe0123 <Jbagostin@gmail.com>
2018-02-02 16:33:30 +0100

    Envois de mail premiere version

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\MailServices;
 use Twig\Environment;
 
 class IndexController extends Controller {
@@ -12,10 +13,13 @@ class IndexController extends Controller {
     /**
      * @Route("/")
      */
-    public function index (Environment $twig){
+    public function index (Environment $twig,MailServices $mailerService){
 
 
-
+        $mail = $mailerService;
+        if ($mail->mailTest()) {
+            dump('ok');
+        }
 
         return new Response($twig->render('base.html.twig'));
     }
