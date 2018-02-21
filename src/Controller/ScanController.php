1a5c1cfb278d0d3f2915fdfad400ce3b08018bb8
Valentin CHEVREAU <valentin.chevreau@hetic.net>
2018-02-21 23:54:23 +0100

    ScanController.php | Migrations

--- src/Controller/ScanController.php
+++ src/Controller/ScanController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Twig\Environment;
 
 class ScanController extends Controller
 {
     /**
      * @Route("/scan", name="scan")
      */
-    public function index()
+    public function index(Environment $twig, Connection $connection)
     {
 
         header("Access-Control-Allow-Origin: *");
