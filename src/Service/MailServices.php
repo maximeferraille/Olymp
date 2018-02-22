272743b277221a0349c48430cb1c2bcb6536ca88
Jibe0123 <Jbagostin@gmail.com>
2018-02-22 15:49:50 +0100

    ajout d'header pour le mail

--- src/Service/MailServices.php
+++ src/Service/MailServices.php
@@ -17,12 +17,18 @@ class MailServices{
 
 
 
-        $message =  $message = (new \Swift_Message('test mail'));
+        $message = (new \Swift_Message('test mail'));
             $message->setFrom('jb@valentinchevreau.fr');
             $message->setTo('jbagostin@gmail.com');
             $message->addPart(
                 'Someone just updated the site. We told them: '
             );
+
+        $headers = $message->getHeaders();
+        $headers->addIdHeader('Message-ID', "b3eb7202-d2f1-11e4-b9d6-1681e6b88ec1@domain.com");
+        $headers->addTextHeader('MIME-Version', '1.0');
+        $headers->addTextHeader('X-Mailer', 'PHP v' . phpversion());
+        $headers->addParameterizedHeader('Content-type', 'text/html', ['charset' => 'utf-8']);
 
         return $this->mailer->send($message) > 0;
     }
